78e27b1fd564762e156ffcf99f0a49f20d2b41e3
karan1310-bit <karanbhati1310@gmail.com>
2024-07-30 21:36:47 +0530

    Update EditPost.jsx

--- client/vite-project/src/pages/EditPost.jsx
+++ client/vite-project/src/pages/EditPost.jsx
@@ -11,7 +11,7 @@ export default function EditPost() {
   const [redirect,setRedirect] = useState(false);
 
   useEffect(() => {
-    fetch('http://localhost:4000/post/'+id)
+    fetch('https://blogbae-backend/post/+id.onrender.com')
       .then(response => {
         response.json().then(postInfo => {
           setTitle(postInfo.title);
